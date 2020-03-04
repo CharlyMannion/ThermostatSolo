e74007b0feb234266c5963fa0b38bd898d85e14c
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-04 15:43:50 +0000

    connect all the html to jquery events

--- jasmine-standalone-3.5.0 (1)/interface.js
+++ jasmine-standalone-3.5.0 (1)/interface.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
   $('#temperature').text(thermostat.temperature + "°C");
 
+  $('#power-saving-status').text(thermostat.powerSavingStatus());
+
   $('#energy-usage-status').text(thermostat.energyUsage());
 
 
@@ -19,5 +21,10 @@ $(document).ready(function() {
   $('#temperature-reset').on('click', function() { // event listener
     thermostat.resetToDefault(); // update model
     $('#temperature').text(thermostat.temperature); // update view
+  });
+
+  $('#toggle_power_saving').on('click', function() { // event listener
+    thermostat.togglePowerSaving(); // update model
+    $('#power-saving-status').text(thermostat.powerSavingStatus()); // update view
   });
 });
