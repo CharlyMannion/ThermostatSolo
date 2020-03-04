1e507662c432594d268222ed3360a5ef1b4230f5
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-04 15:26:11 +0000

    adds temp up function interface

--- jasmine-standalone-3.5.0 (1)/interface.js
+++ jasmine-standalone-3.5.0 (1)/interface.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
 
   $('#temperature').text(thermostat.temperature + "°C");
-})
+
+  $('#temperature-up').on('click', function() { // event listener
+    thermostat.increaseTemp(); // update model
+    $('#temperature').text(thermostat.temperature); // update view
+  });
+});
