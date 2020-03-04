9df404dee33b080d1360d6bbb9f76b853d2cd9d4
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-04 17:33:29 +0000

    adds API

--- jasmine-standalone-3.5.0 (1)/interface.js
+++ jasmine-standalone-3.5.0 (1)/interface.js
@@ -42,5 +42,33 @@ $(document).ready(function() {
   function updateEnergyUsage() {
     $('#energy-usage-status').text(thermostat.energyUsage());
     $('#energy-usage-status').attr('class', thermostat.energyUsage());
-  }
+  };
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
+
+  // function displayWeather(city) {
+  //  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  //  var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+  //  var units = '&units=metric';
+  //  $.get(url + token + units, function(data) {
+  //    $('#current-temperature').text(data.main.temp);
+  //  });
+  //
+  // displayWeather('London');
+  //
+  // $('#select-city').submit(function(event) {
+  //   event.preventDefault();
+  //   var city = $('#current-city').val();
+  //   displayWeather(city);
+  // });
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  });
 });
