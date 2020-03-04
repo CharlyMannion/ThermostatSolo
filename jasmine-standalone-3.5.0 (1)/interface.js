1478b14ae02374fdcbedfd6794d78cc294dd8cb9
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-04 15:55:35 +0000

    adds some css and refactors

--- jasmine-standalone-3.5.0 (1)/interface.js
+++ jasmine-standalone-3.5.0 (1)/interface.js
@@ -5,35 +5,41 @@ $(document).ready(function() {
 
   updatePowerSaving();
 
-  $('#energy-usage-status').text(thermostat.energyUsage());
-
+  updateEnergyUsage();
 
   $('#temperature-up').on('click', function() {
     thermostat.increaseTemp();
     updateTemperature();
+    updateEnergyUsage();
   });
 
   $('#temperature-down').on('click', function() {
     thermostat.decreaseTemp();
     updateTemperature();
+    updateEnergyUsage();
   });
 
   $('#temperature-reset').on('click', function() {
     thermostat.resetToDefault();
     updateTemperature();
+    updateEnergyUsage();
   });
 
   $('#toggle_power_saving').on('click', function() {
     thermostat.togglePowerSaving();
     updatePowerSaving();
-    // $('#power-saving-status').text(thermostat.powerSavingStatus());
   });
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature + "°C");
+    $('#temperature').attr('class', thermostat.energyUsage());
   };
 
   function updatePowerSaving() {
     $('#power-saving-status').text(thermostat.powerSavingStatus());
   };
+
+  function updateEnergyUsage() {
+    $('#energy-usage-status').text(thermostat.energyUsage());
+  }
 });
