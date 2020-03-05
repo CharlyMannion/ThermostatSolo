165ca3d733df9a58862b2596bce1489761894b0d
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-05 11:35:50 +0000

    refactors get function

--- jasmine-standalone-3.5.0 (1)/interface.js
+++ jasmine-standalone-3.5.0 (1)/interface.js
@@ -47,7 +47,10 @@ $(document).ready(function() {
   $('#select-city').submit(function(event) {
     event.preventDefault();
     var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
       $('#current-temperature').text(data.main.temp + "°C");
     });
   });
